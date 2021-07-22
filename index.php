523c764f2fd9333e58c819ae080a281e20015dc6
Vanelle Totsop <tiotsopvany01@gmail.com>
2021-07-22 13:29:37 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Syteme de gestion de la formation ADDA START-UP</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="..\Assets\bootstrap4.min.css" />
+	<style type="text/css"></style>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div id="logo" class="col-md-3">ADDA'S</div>
+			<div class="col-md-5">
+				<ul>
+					<li><a href="">Accueill</a></li>
+					<li><a href="">Se former</a></li>
+					<li><a href="">Forum</a></li>
+					<li><a href="">Nos formations</a></li>
+					<li><a href="">Aide</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+    <div class="container">
+    	<h4>Bienvenue a tous sur ADDA'S FORM pour vivre une grande experience en programmation informatique</h4>
+    	<div class="row">
+    		<div class="col-md-3">
+    			<div><a href=""><img src=""></a></div>
+    			<div class="title"><a href="">Formations en java</a></div>
+    			<button class="btn btn-info">Commencer</button>
+    		</div>
+    		<div class="col-md-3">
+    			<div><a href=""><img src=""></a></div>
+    			<div class="title"><a href="">Formations en Html et css</a></div>
+    			<button class="btn btn-info">Commencer</button>
+    		</div>
+    		<div class="col-md-3">
+    			<div><a href=""><img src=""></a></div>
+    			<div class="title"><a href="">Formations en PHP</a></div>
+    			<button class="btn btn-info">Commencer</button>
+    		</div>
+    	</div>
+    	<div class="row">
+    		<div class="col-md-6">
+    			<button class="btn btn-info">Voir plus de formation</button>
+    		</div>
+    	</div>
+    </div>
+</body>
+</html>
